5a5932f70764679658ef6035e24f97d4dd6c0405
GootaLK <lokosmen1235@gmail.com>
2022-02-17 23:33:15 -0500

    Feat: Add Pexels icon to Header component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,25 @@
 import './Header.css'
 
+import pexelsSrc from '../../assets/pexels-icon.svg'
+
 export const Header = () => {
   return (
     <header className='Header'>
-      <div className="Header-logo">
+      <div className='Header-logo'>
         <h1>Gallery</h1>
       </div>
-      <div className="Header-search_wrapper">
-        <input className='Header-search_input' type="search" placeholder='Search images'/>
+      <div className='Header-search_wrapper'>
+        <input className='Header-search_input' type='search' placeholder='Search images'/>
+      </div>
+      <div className='Header-pexels_link'>
+        <a
+          href='https://pexels.com'
+          target='_blank'
+          rel='noreferrer noopener'
+          title='Photos provided by Pexels'
+        >
+          <img src={pexelsSrc} alt="Pexels" />
+        </a>
       </div>
     </header>
   )
