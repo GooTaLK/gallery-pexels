c66c1d226fa07185f5b777e89fc1b5ed19c7b1a1
GootaLK <lokosmen1235@gmail.com>
2022-02-26 20:18:09 -0500

    Feat: Add effect to close Modal when press Esc key or click overlay

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,12 +1,31 @@
+import React, { useEffect, useRef } from 'react'
 import './Modal.css'
 
-export const Modal = ({ open = false, children }) => {
+export const Modal = ({ open = false, toClose, children }) => {
   if (!open) return null
+
+  const modal = useRef()
+
+  function handleKeydownEsc (e) {
+    if (e.key !== 'Escape') return
+    toClose()
+  }
+
+  useEffect(() => {
+    modal.current.focus()
+  }, [open])
 
   return (
     <>
-      <div className="Modal-overlay" />
-      <div className='Modal'>{children}</div>
+      <div className='Modal-overlay' onClick={toClose}/>
+      <div
+        className='Modal'
+        onKeyDown={handleKeydownEsc}
+        tabIndex='0'
+        ref={modal}
+      >
+        {children}
+      </div>
     </>
   )
 }
