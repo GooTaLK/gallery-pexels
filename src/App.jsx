2549084280c214be13934daaacad46a0da2a92f5
GootaLK <lokosmen1235@gmail.com>
2022-03-01 22:02:22 -0500

    Feat: Add images data whit Pexels API

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
+import React, { useEffect, useState } from 'react'
 import './App.css'
 
 import { Header } from './components/Header'
 import { Mosaic } from './components/Mosaic'
 
+import { getCuratedPhotos } from './pexelsAPI'
+
 function App () {
+  const [newCuratedImages, setNewCuratedImages] = useState(null)
+
+  useEffect(() => {
+    getCuratedPhotos().then(({ photos }) => setNewCuratedImages(photos))
+  }, [])
+
   return (
     <div className='App'>
       <Header />
       <main>
-        <Mosaic />
+        <Mosaic newImages={newCuratedImages} />
       </main>
     </div>
   )
