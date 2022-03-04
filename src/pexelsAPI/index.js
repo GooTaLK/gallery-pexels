bbcd72f46a402a7cf6da253fdde4d0fc690ee7a3
GootaLK <lokosmen1235@gmail.com>
2022-03-03 21:46:22 -0500

    Feat: Add page parameter for pexelAPI functions

--- src/pexelsAPI/index.js
+++ src/pexelsAPI/index.js
@@ -11,12 +11,12 @@ async function fetchPexelsAPI (url) {
   }
 }
 
-export async function getCuratedPhotos ({ perPage = 80 } = {}) {
-  return await fetchPexelsAPI(`https://api.pexels.com/v1/curated?per_page=${perPage}`)
+export async function getCuratedPhotos ({ page = 1, perPage = 80 } = {}) {
+  return await fetchPexelsAPI(`https://api.pexels.com/v1/curated?page=${page}&per_page=${perPage}`)
 }
 
-export async function searchPhotos ({ query, perPage = 80 }) {
-  return await fetchPexelsAPI(`https://api.pexels.com/v1/search?query=${query}&per_page=${perPage}`)
+export async function searchPhotos ({ query, page = 1, perPage = 80 }) {
+  return await fetchPexelsAPI(`https://api.pexels.com/v1/search?query=${query}&page=${page}per_page=${perPage}`)
 }
 
 export async function getPhoto ({ id }) {
