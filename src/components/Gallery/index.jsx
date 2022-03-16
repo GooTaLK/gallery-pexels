13a06b200fa3164560bb7194caf7dfcea882be38
GootaLK <lokosmen1235@gmail.com>
2022-03-16 13:23:20 -0500

    Feat: Extract render from Home component to Gallery component

---
+++ src/components/Gallery/index.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect } from 'react'
+import { usePexelsData } from '../../hooks/usePexelsData'
+
+import { Mosaic } from '../Mosaic'
+
+export const Gallery = ({ promisseCB }) => {
+  const {
+    data: media,
+    loading: photosLoading,
+    updateData: updatePhotos
+  } = usePexelsData({ promisseCB })
+
+  function handleMainScroll (e) {
+    if (media.completed || photosLoading) return
+
+    const EXTRA_CLIENT_HEIGHT = 3
+    if (e.target.scrollTop < e.target.scrollHeight - (e.target.clientHeight * (1 + EXTRA_CLIENT_HEIGHT))) return
+
+    updatePhotos()
+  }
+
+  useEffect(() => {
+    updatePhotos()
+  }, [])
+
+  return (
+    <main className='App-main' onScroll={handleMainScroll} >
+      <Mosaic images={media.data} />
+    </main>
+  )
+}
--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,33 +1,9 @@
-import React, { useEffect } from 'react'
-import { usePexelsData } from '../../hooks/usePexelsData'
-
-import { Mosaic } from '../Mosaic'
+import { Gallery } from '../Gallery'
 
 import { getCuratedPhotos } from '../../pexelsAPI'
 
 export const Home = () => {
-  const {
-    data: photos,
-    loading: photosLoading,
-    updateData: updatePhotos
-  } = usePexelsData({ promisseCB: async (page) => await getCuratedPhotos({ page }) })
-
-  function handleMainScroll (e) {
-    if (photos.completed || photosLoading) return
-
-    const EXTRA_CLIENT_HEIGHT = 3
-    if (e.target.scrollTop < e.target.scrollHeight - (e.target.clientHeight * (1 + EXTRA_CLIENT_HEIGHT))) return
-
-    updatePhotos()
-  }
-
-  useEffect(() => {
-    updatePhotos()
-  }, [])
-
   return (
-    <main className='App-main' onScroll={handleMainScroll} >
-      <Mosaic images={photos.data} />
-    </main>
+    <Gallery promisseCB={async (page) => await getCuratedPhotos({ page })}/>
   )
 }
