c0b13a30155743591d5b3c6c5130c6bfc0f89954
GootaLK <lokosmen1235@gmail.com>
2022-03-20 21:59:52 -0500

    Refactor: Extract promisseCB value from Search component in a named function (searchCallback)

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -7,7 +7,11 @@ import { searchPhotos } from '../../pexelsAPI'
 export const Search = () => {
   const { query } = useParams()
 
+  async function searchCallback (page) {
+    return await searchPhotos({ page, query })
+  }
+
   return (
-    <Gallery promisseCB={async (page) => searchPhotos({ page, query })} />
+    <Gallery promisseCB={searchCallback} />
   )
 }
