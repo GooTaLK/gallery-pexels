26e02183470c836cb566a3df9ce244c45070a3d8
GootaLK <lokosmen1235@gmail.com>
2022-03-20 23:16:31 -0500

    Feat: Add functionality to search input

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,15 +1,40 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
 import './Header.css'
 
 import pexelsSrc from '../../assets/pexels-icon.svg'
 
 export const Header = () => {
+  const [searchValue, setSearchValue] = useState('')
+
+  const navigate = useNavigate()
+
+  function handleSearchValue (e) {
+    setSearchValue(e.target.value)
+  }
+
+  function handleSearchInput (e) {
+    if (e.key !== 'Enter') return
+
+    const query = e.target.value
+    query !== '' && navigate(`/search/${query}`)
+  }
+
   return (
     <header className='Header App-Header'>
       <div className='Header-logo'>
         <h1>Gallery</h1>
       </div>
       <div className='Header-search_wrapper'>
-        <input className='Header-search_input' type='search' placeholder='Search images'/>
+        <input
+          className='Header-search_input'
+          type='search'
+          placeholder='Search images'
+          value={searchValue}
+          onInput={handleSearchValue}
+          onKeyDown={handleSearchInput}
+        />
       </div>
       <div className='Header-pexels_link'>
         <a
