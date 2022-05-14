012dec8989a844d20beb8a38c7e3439b03b84b7e
GootaLK <lokosmen1235@gmail.com>
2022-05-13 21:24:39 -0500

    Fix: Add alternative text when it does not exist on Column figures

--- src/components/Column/index.jsx
+++ src/components/Column/index.jsx
@@ -1,19 +1,25 @@
 import './Column.css'
 
+const ALT_TEXT = 'Photo provided by Pexels'
+
 export const Column = ({ items, onClickItem }) => {
   return (
     <div className='Column'>
     {
-      items.map(({ id, src, alt }) => (
-        <figure
-          className='Column-item'
-          onClick={() => onClickItem(id)}
-          key={`item--${id}`}
-        >
-          <figcaption>{alt}</figcaption>
-          <img src={src.medium} alt={alt} loading='lazy' />
-        </figure>
-      ))
+      items.map(({ id, src, alt: itemAlt }) => {
+        const alt = itemAlt || ALT_TEXT
+
+        return (
+          <figure
+            className='Column-item'
+            onClick={() => onClickItem(id)}
+            key={`item--${id}`}
+          >
+            <figcaption>{alt}</figcaption>
+            <img src={src.medium} alt={alt} loading='lazy' />
+          </figure>
+        )
+      })
     }
     </div>
   )
